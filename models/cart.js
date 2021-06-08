024b808b988ff42b90094650b6d32d9f8e63f180
Vamsi027 <you@example.com>
2021-06-08 12:52:23 +0530

    Query params

--- models/cart.js
+++ models/cart.js
@@ -31,5 +31,33 @@ module.exports=class Cart{
             fs.writeFile(p,JSON.stringify(cart),err=>{})
         })
     }
+    static deleteProduct(id,productPrice){
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                return;
+            }
+            const updatedCart={...JSON.parse(fileContent)};
+            const product=updatedCart.products.find(prod=>prod.id===id)
+            const productQty=product.qty;
+            updatedCart.products=updatedCart.products.filter(
+                prod=>prod.id!==id
+            )
+            updatedCart.totalPrice=updatedCart.totalPrice-productPrice * productQty;
+            fs.writeFile(p,JSON.stringify(updatedCart),err=>{})
+        })
+    }
+
+    static getCart(cb){
+        fs.readFile(p,(err,fileContent)=>{
+            const cart=JSON.parse(fileContent);
+            if(err){
+                cb(null)
+            }
+            else
+            {
+            cb(cart)
+            }
+        })
+    }
 
 }
