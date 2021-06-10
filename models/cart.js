de5d6cb9dea0c25e720a5965c05f778e03d5ca4a
Vamsi027 <you@example.com>
2021-06-10 10:00:00 +0530

    database

--- models/cart.js
+++ models/cart.js
@@ -38,6 +38,9 @@ module.exports=class Cart{
             }
             const updatedCart={...JSON.parse(fileContent)};
             const product=updatedCart.products.find(prod=>prod.id===id)
+            if(!prod){
+                return;
+            }
             const productQty=product.qty;
             updatedCart.products=updatedCart.products.filter(
                 prod=>prod.id!==id
