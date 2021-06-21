14d6718b024e633f5ba592568d996ae57de60300
Vamsi027 <you@example.com>
2021-06-21 11:01:13 +0530

    sequelize

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize=require('./util/database')
 
 const app = express();
 
@@ -21,4 +22,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(4000);
+sequelize.sync().then(result=>{app.listen(4000);}).catch(err=>{})
--- util/database.js
+++ util/database.js
@@ -1,10 +1,5 @@
-const mysql=require('mysql2')
+const Sequelize=require('sequelize')
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'vamsi027'
-})
+const sequelize=new Sequelize('node-complete','root','vamsi027',{dialect:'mysql',host:'localhost'});
 
-module.exports=pool.promise();
+module.exports=sequelize;
